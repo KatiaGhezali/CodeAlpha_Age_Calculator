f4d5c7d5c433059e97de47f25033d99ed435d11c
Katia Ghezali <ghezalikatia@hotmail.com>
2023-11-10 18:06:11 +0100

    Add functionnalities in case the user does not enter a date of birth and requests their age

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ function calculateAge() {
     const dobInput = document.getElementById('dob');
     const result = document.getElementById('result');
 
+
+    if (!dobInput.value) {
+        result.innerText = 'Please enter a valid date of birth';
+        return;
+    }
+
     const dob = new Date(dobInput.value);
     const today = new Date();
 
